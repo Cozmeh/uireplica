86db7455a6be6aa011ae4016c47f105e760908cc
Cozmeh <hemsainath15@gmail.com>
2024-05-18 20:55:08 +0530

    made barData final

--- lib/Domain/data_classes/sales_report_bardata.dart
+++ lib/Domain/data_classes/sales_report_bardata.dart
@@ -8,7 +8,7 @@ class SalesReportBarData {
   final double thursdayAmount;
   final double fridayAmount;
   final double saturdayAmount;
-  List<WeekDayBar> barData = [];
+  final List<WeekDayBar> barData;
 
   SalesReportBarData(
       {required this.sundayAmount,
@@ -17,15 +17,14 @@ class SalesReportBarData {
       required this.wednesdayAmount,
       required this.thursdayAmount,
       required this.fridayAmount,
-      required this.saturdayAmount}) :
-        barData = [
-      WeekDayBar(x: 0, y: sundayAmount),
-      WeekDayBar(x: 1, y: mondayAmount),
-      WeekDayBar(x: 2, y: tuesdayAmount),
-      WeekDayBar(x: 3, y: wednesdayAmount),
-      WeekDayBar(x: 4, y: thursdayAmount),
-      WeekDayBar(x: 5, y: fridayAmount),
-      WeekDayBar(x: 6, y: saturdayAmount)
-    ];
-      
+      required this.saturdayAmount})
+      : barData = [
+          WeekDayBar(x: 0, y: sundayAmount),
+          WeekDayBar(x: 1, y: mondayAmount),
+          WeekDayBar(x: 2, y: tuesdayAmount),
+          WeekDayBar(x: 3, y: wednesdayAmount),
+          WeekDayBar(x: 4, y: thursdayAmount),
+          WeekDayBar(x: 5, y: fridayAmount),
+          WeekDayBar(x: 6, y: saturdayAmount)
+        ];
 }
